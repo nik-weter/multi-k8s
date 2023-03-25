b57472722d8c160703d6030caee6a404754dd5f5
nik-weter <nik-weter@yandex.ru>
2023-03-25 09:46:44 +0700

    Add kubernetes config

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-if [ -e ./complex/.git ]
-then
-cd complex && git pull
-docker-compose up -d
-else
-mkdir complex && cd complex && git clone git@github.com:nik-weter/multi-docker.git .
-docker-compose up -d
-fi
